5a220d73e4e2c9e6b3eae480f85c6c6ea9d9f0fe
Ganben <caozhao@gmail.com>
2018-09-15 15:59:25 +0800

    34

--- algorithm_problems/34-find_1st_and_last_position_in_sorted_array.py
+++ algorithm_problems/34-find_1st_and_last_position_in_sorted_array.py
@@ -28,21 +28,80 @@ class Solution:
         :type target: int
         :rtype: List[int]
         """
-        
+        delta = 0
+        step = 0
+        ptr = 0
+        result = [-1, -1]
+        for i in range(int(math.log(len(nums), 2))+3):
+            d = target - nums[ptr]
+            if d < 0:
+                #modify step
+                step = int(len(nums)/pow(2,i+1))
+                if step < 1:
+                    step = 1
+                delta = d
+                ptr -= step
+            elif d > 0:
+                #modify step
+                step = int(len(nums)/pow(2, i+1))
+                if step == 0:
+                    step = 1
+                delta = d
+                ptr += step
+                
+            elif d == 0 and result[0] == -1:
+                # 4 cases: left end, middle and right end;
+                if ptr == 0:
+                    result[0] = ptr
+                    continue
+                elif ptr > 0 and nums[ptr - 1] != target:
+                    result[0] = ptr
+                    continue
+                    # not change ptr
+                else:
+                    
+                # not left end, step decide
+                    step = int(len(nums)/pow(2,i+3))
+                    if step == 0:
+                        step = 1
+                    delta = 0
+                    ptr -= step
+                
+                
+            elif d == 0 and result[0] != -1:
+                # find end
+                if ptr == len(nums) -1:
+                    result[1] = ptr
+                    break
+                elif ptr < len(nums) - 1 and nums[ptr+1] != target:
+                    result[1] = ptr
+                    break
+                
+                #not right end, step dicide
+                step = int(len(nums)/pow(2,i+3))
+                if step < 1:
+                    step = 1
+                delta = 0
+                ptr += step
+            else:
+                ptr += 1
+            
+        return result
 
 class Test(unittest.TestCase):
     def test1(self):
         self.assertEqual(
             Solution().searchRange(
-                ,
-
-            )
+                [5,7,7,8,8,10],
+                8
+            ),
             [3,4]
         )
     def test2(self):
         self.assertEqual(
             Solution().searchRange(
-                ,
+                [5,7,7,8,8,10],
+                6
             ),
             [-1,-1]
         )
