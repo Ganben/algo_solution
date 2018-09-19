7df71f23df69d0685c5264bfc04c3e3d9582bfd6
Ganben <caozhao@gmail.com>
2018-09-19 23:27:21 +0800

    ??

---
+++ algorithm_problems/40-combination_sum2.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# ganben
+#
+
+import unittest
+import math
+'''
+https://leetcode.com/problems/combination-sum-ii/description/
+Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sums to target.
+
+Each number in candidates may only be used once in the combination.
+
+Note:
+
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations.
+Example 1:
+
+Input: candidates = [10,1,2,7,6,1,5], target = 8,
+A solution set is:
+[
+  [1, 7],
+  [1, 2, 5],
+  [2, 6],
+  [1, 1, 6]
+]
+Example 2:
+
+Input: candidates = [2,5,2,1,2], target = 5,
+A solution set is:
+[
+  [1,2,2],
+  [5]
+]
+'''
+class Solution:
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        
+class Test(unittest.TestCase):
+    def test1(self):
+        
---
+++ algorithm_problems/41-first_missing_positive.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# ganben
+#
+
+import unittest
+import math
+'''
+https://leetcode.com/problems/first-missing-positive/description/
+Given an unsorted integer array, find the smallest missing positive integer.
+
+Example 1:
+
+Input: [1,2,0]
+Output: 3
+Example 2:
+
+Input: [3,4,-1,1]
+Output: 2
+Example 3:
+
+Input: [7,8,9,11,12]
+Output: 1
+Note:
+
+Your algorithm should run in O(n) time and uses constant extra space.
+'''
+class Solution:
+    def firstMissingPositive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+
+class Test(unittest.TestCase):
+    def test1(self):
+        
