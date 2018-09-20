c5ceb44a174e0d4096f6667614edd0871dbde451
Ganben <caozhao@gmail.com>
2018-09-20 20:38:16 +0800

    ??

--- algorithm_problems/41-first_missing_positive.py
+++ algorithm_problems/41-first_missing_positive.py
@@ -34,4 +34,19 @@ class Solution:
 
 class Test(unittest.TestCase):
     def test1(self):
-        
+        self.assertEqual(
+            Solution().firstMissingPositive(
+                [1,2,0]
+            ),
+            3
+        )
+    def test2(self):
+        self.assertEqual(
+            Solution().firstMissingPositive([3,4,-1,1]),
+            2
+        )
+    def test3(self):
+        self.assertEqual(
+            Solution().firstMissingPositive([7,8,9,11,12]),
+            1
+        )
