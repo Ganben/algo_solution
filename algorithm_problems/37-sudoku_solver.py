6df636765057cee9c6f9081eb10669f8566817f9
Ganben <caozhao@gmail.com>
2018-09-23 23:05:34 +0800

    fx1

--- algorithm_problems/37-sudoku_solver.py
+++ algorithm_problems/37-sudoku_solver.py
@@ -20,13 +20,64 @@ The given board contain only digits 1-9 and the character '.'.
 You may assume that the given Sudoku puzzle will have a single unique solution.
 The given board size is always 9x9.
 '''
+class Checker:
+    def __init__(self):
+        self.dictionary = {}
+    
+    def check(self, num):
+        if self.dictionary.get(num) is None:
+            self.dictionary[num] = True
+            return True
+        elif num == '.':
+            return True
+        else:
+            return False
+
+
 class Solution:
     def solveSudoku(self, board):
         """
         :type board: List[List[str]]
         :rtype: void Do not return anything, modify board in-place instead.
         """
+        return 
         
+    def find_available(self, board, row, col):
+        # find a available set for row col:
+        st = set([1,2,3,4,5,6,7,8,9])
+        return st
+        
+    def boxverify(self, board):
+        for i in range(0, len(board)-2, 3):
+            
+            for j in range(0, len(board)-2, 3):
+                # 3x3 check
+                checker = Checker()
+                for ii in range(3):
+                    for jj in range(3):
+                        if not checker.check(board[i+ii][j+jj]):
+                            return False
+
+        return True
+    
+    def rowverify(self, board):
+        for i in range(len(board)):
+            for j in range(len(board)):
+                # row check
+                checker = Checker()
+                if not checker.check(board[i][j]):
+                    return False
+        return True
+
+    def colverify(self, board):
+        for i in range(len(board)):
+            for j in range(len(board)):
+                # col check
+                checker = Checker()
+                if not checker.check(board[j][i]):
+                    return False
+        
+        return True
 
 class Test(unittest.TestCase):
     def test1(self):
