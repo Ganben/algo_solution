63a38e073e23c67ecf12b2b95488b87cef702f03
Ganben <caozhao@gmail.com>
2018-09-25 21:20:24 +0800

    37 try

--- algorithm_problems/37-sudoku_solver.py
+++ algorithm_problems/37-sudoku_solver.py
@@ -40,12 +40,77 @@ class Solution:
         :type board: List[List[str]]
         :rtype: void Do not return anything, modify board in-place instead.
         """
+        i = 0
+        j = 0
+        ans = board.copy()
+        potentials = []
+        pos = 0
+        while self.is_completed(ans) is not True:
+            i, j = self.is_completed(ans)
+            pot = []
+            valids = self.find_available(ans, i, j)
+            if len(valids) > 0:
+                pot.append(i)
+                pot.append(j)
+                ans[i][j] == valids[0]
+                valids.pop(0)
+                pot.append(valids)
+                potentials.append(pot)
+            else:
+                for ii in range(potentials):
+                    vvs = potentials[len(potentials) - ii]
+                    if len(vvs) > 2:
+                        i = vvs[0]
+                        j = vvs[1]
+                        ans[i][j] == vvs.pop(2)
+                    else:
+                        continue
+                    
         return 
-        
+
+    def is_completed(self, board):
+        #
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == '.':
+                    return i, j
+        return True
+    
     def find_available(self, board, row, col):
         # find a available set for row col:
         st = set([1,2,3,4,5,6,7,8,9])
-        return st
+        bi = row - (row % 3)
+        bj = col = (col % 3)
+
+        # box remove:
+        for ii in range(3):
+            for jj in range(3):
+                if board[bi+ii][bj+jj] == '.':
+                    continue
+                elif int(board[bi+ii][bj+jj]) in st:
+                    st.remove(int(board[bi+ii][bj+jj]))
+                else:
+                    continue
+
+        # row remove:
+        for i in range(9):
+            if board[row][i] == '.':
+                continue
+            elif int(board[row][i]) in st:
+                st.remove(int(board[row][i]))
+            else:
+                continue
+        
+        # col remove:
+        for i in range(9):
+            if board[i][col] == '.':
+                continue
+            elif int(board[i][col]) in st:
+                st.remove(int(board[i][col]))
+            else:
+                continue
+        
+        return list(st)
         
     def boxverify(self, board):
         for i in range(0, len(board)-2, 3):
